7835214db9723bb3bef84bcbc20eee84cb5aa666
sergeinikonov <sergei.nikonov.ua@gmail.com>
2021-07-23 16:27:38 +0300

    [AOMA-16473] dotenv and db connection setup done

---
+++ be/src/config.ts
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+if (!process.env.DB_HOST) {
+  throw Error('DB_HOST is not defined');
+}
+
+if (!process.env.DB_NAME) {
+  throw Error('DB_NAME is not defined');
+}
+
+if (!process.env.DB_USER) {
+  throw Error('DB_USER is not defined');
+}
+
+if (!process.env.DB_PASSWORD) {
+  throw Error('DB_PASSWORD is not defined');
+}
+
+module.exports = {
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+};
