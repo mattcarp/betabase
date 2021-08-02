3c83a8d70b00010a2c922f6ad2bd6176d117db9c
gord004 <andrey.gordienko.intimetechnologies@sonymusic.com>
2021-08-02 14:40:03 +0300

    [BB] add material and assets

--- fe/src/app/app.component.ts
+++ fe/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+declare let require: any;
+const fs = require('../../package.json');
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'thebetabase3';
+  private appTitle = 'Beta Base';
+  private build = '';
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle(this.appTitle);
+
+    const style = [
+      'padding: 4px 10px;',
+      'background: linear-gradient(#587887, #57a0bf);',
+      'font: normal 12px/1 Roboto, Helvetica, Arial;',
+      'color: #fff;',
+      'border-radius: 3px;',
+    ].join('');
+
+    console.log(
+      `%c ${this.appTitle} %c ${this.isBuildDev ? '❗localhost' : ''} %c ${this.build} UI v.${fs.version}`,
+      style,
+      '',
+      '',
+    );
+  }
+
+  get isBuildDev(): boolean {
+    const isDev = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+    if (isDev) {
+      this.build = '[DEV]';
+    } else {
+      this.build = '';
+    }
+    return isDev;
+  }
 }
