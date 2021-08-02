26af523429e067c93c3d237140be41ab3b648b7d
gord004 <andrey.gordienko.intimetechnologies@sonymusic.com>
2021-08-02 14:40:03 +0300

    [BB] add material and assets

--- fe/src/app/app.module.ts
+++ fe/src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const appearance: MatFormFieldDefaultOptions = {
+  appearance: 'outline',
+};
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: appearance,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
