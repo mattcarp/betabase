e0f894c719b4f70d4e94885ca9c102660ec4908b
gord004 <andrey.gordienko.intimetechnologies@sonymusic.com>
2021-08-11 17:33:03 +0300

    [BB] update Support page

--- fe/src/app/support/support.component.ts
+++ fe/src/app/support/support.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
 
+import { SupportConstants } from './support.constant';
+
 @Component({
   selector: 'app-support',
   templateUrl: './support.component.html',
   styleUrls: ['./support.component.scss'],
   host: { '[class.page]': 'true' },
 })
-export class SupportComponent {}
+export class SupportComponent {
+  isSetupConference = false;
+  isSendEmail = false;
+  selectedContacts: { email: string; name: string; phone: string; isChecked: boolean }[] = [];
+  contacts = SupportConstants.contacts;
+
+  onSetupConferenceClick(): void {
+    this.isSetupConference = !this.isSetupConference;
+  }
+
+  onChangeContact(isChecked: boolean, id: number): void {
+    this.contacts[id].isChecked = isChecked;
+    this.selectedContacts = this.contacts.filter((item: any) => item.isChecked);
+  }
+
+  onChangeSendEmail(isChecked: boolean): void {
+    this.isSendEmail = isChecked;
+  }
+}
--- fe/src/app/support/support.module.ts
+++ fe/src/app/support/support.module.ts
@@ -1,13 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { NavModule } from '../shared/layout/nav/nav.module';
 import { SupportRoutingModule } from './support-routing.module';
 import { SupportComponent } from './support.component';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [SupportComponent],
-  imports: [CommonModule, NavModule, SupportRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    NavModule,
+    SupportRoutingModule,
+    MatChipsModule,
+    MatCheckboxModule,
+  ],
   exports: [SupportComponent],
 })
 export class SupportModule {}
