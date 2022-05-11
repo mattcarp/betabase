f69a08e0d80be2ca3f885e62311c573f8b109913
gord004 <andrey.gordienko.intimetechnologies@sonymusic.com>
2022-05-11 17:33:09 +0300

    [BB] fix comments

--- packages/fe/src/app/dashboard/app-details/chart/chart.component.ts
+++ packages/fe/src/app/dashboard/app-details/chart/chart.component.ts
@@ -10,14 +10,17 @@ import { ChartOption } from './chart.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChartComponent {
-  @Input() set inputOptions(options: ChartOption) {
-    this.chartType = options.type;
-    this.title = options.title;
-    this.chartData.labels = options.labels;
-    this.chartData.datasets = options.datasets;
-    this.result = options.result ? `${options.result}%` : '';
+  @Input() set inputOptions(options: ChartOption | undefined) {
+    if (options) {
+      this.chartType = options.type;
+      this.title = options.title;
+      this.chartData.labels = options.labels;
+      this.chartData.datasets = options.datasets;
+      this.result = options.result ? `${options.result}%` : '';
+    }
   }
-  title = '';
+
+  title: string | undefined = '';
   result = '';
   chartLegend = true;
   chartType: ChartType = 'pie';
